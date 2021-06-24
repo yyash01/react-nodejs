dcceedab23d5676a2b5b5b6b3b9ff9177ab97268
YashJain <yjain3.436@gmail.com>
2021-06-24 19:44:55 +0530

    done with frontend firstly

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>CRUD Application</h1>
+
+      <div className="form">
+        <label>MovieName</label>
+        <input type="text" name="movieName"/>
+        <label>Review</label>
+        <input type="text" name="review"/>
+        <button>Submit</button>
+      </div> 
+    </div>
+  );
+}
+
+export default App;

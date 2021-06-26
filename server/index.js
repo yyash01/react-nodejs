30354187112de1c5a0bb1b3c9f45ea4b5d073457
YashJain <yjain3.436@gmail.com>
2021-06-26 13:26:29 +0530

    connected - first - time the database.

--- server/index.js
+++ server/index.js
@@ -1,8 +1,19 @@
 const express = require('express')// created a variable for express.
 const app = express()
+const mysql = require('mysql')
+
+const db = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'password@123',
+    database: 'CRUD_DB'
+})
 
 app.get('/',(rec,res) => {
-    res.send('Hello - from Yash JAIN Xd');
+    const sqlInsert = "INSERT INTO MovieReviews(MovieName,MovieReview) VALUES ('Conjuring','Horror Movie');"
+    db.query(sqlInsert,(err,result) => {
+        res.send('Hello from LNMIIT');
+    })
 });
 
 app.listen(3001, ()=> {
